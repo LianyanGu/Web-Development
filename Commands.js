3258d3619038403263e3917927401890e713ccb7
LianyanGu <lianyan.gu@gatech.edu>
2016-11-17 11:09:19 -0500

    Add files via upload

--- Commands.js
+++ Commands.js
@@ -1,33 +1,61 @@
-<script>
-    var options={
-        year:undefined,
-        language:undefined,
-        name:undefined
-    };
-function myClick(){
-    console.log('Coucou Lianyan');
-alert('hi');
+var options = {
+    year: undefined,
+    language: undefined,
+    name: undefined
+};
+
+function onClick() {
+    console.log('asdf');
 }
 
-function changeYear(y){
-    options.year=y;
+function changeYear(y) {
+    options.year = y;
 }
-    function test(selectBox) {
-        var op = selectBox.options[selectBox.selectedIndex];
-        console.log(op);
-        options.language = op.value;
-    }
-function getTrailers(){
+
+function test(selectBox) {
+    var op = selectBox.options[selectBox.selectedIndex];
+    console.log(op);
+    options.language = op.value;
+}
+//in html code, write test(this)
+
+function getTrailers() {
     options.name = document.getElementById("movieName").value;
     console.log('I am going to run the research logic function with the following parameters:\n');
     console.log(options);
 }
-</script>
 
-<script>
-    function findMovieName(){
-        var movieName = document.getElementById("movieName").value;
-        document.writeln(movieName);
-        console.log('Coucou Lianyan');
+
+
+function findMovieName() {
+    var movieName = document.getElementById("movieName").value;
+    document.writeln(movieName);
+    console.log('');
+}
+
+function langChg(){
+    var sel=document.getElementById("lang")
+    function getSelectedOption(sel){
+        var opt;
+        for (var i=0,len=sel.options.length; i<len;i++){
+            opt=sel.options[i];
+            if (opt.seleted===true){
+                break;
+            }
+        }
     }
-</script>
+    console.log(sel.options[sel.selectedIndex].text)
+
+}
+// var sel=document.getElementById("scripts")
+// var opt=sel.options[se1.selectedIndex]
+// consol.log(opt.value)
+
+
+
+
+
+
+
+
+
